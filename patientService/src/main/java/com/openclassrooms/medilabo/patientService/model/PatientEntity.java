209bf586a81e449b33f01cb50f7e149f3a94e6e6
Cy\Cy <marion.lauzier.ad@gmail.com>
2025-06-25 09:55:22 +0200

    connection with mysql database

--- patientService/src/main/java/com/openclassrooms/medilabo/patientService/model/PatientEntity.java
+++ patientService/src/main/java/com/openclassrooms/medilabo/patientService/model/PatientEntity.java
@@ -1,15 +1,13 @@
 package com.openclassrooms.medilabo.patientService.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "patients")
 public class PatientEntity {
 
     @Id
