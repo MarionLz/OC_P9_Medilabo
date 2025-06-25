d4af50a0187f365f845e3b6270062d07a763598f
Cy\Cy <marion.lauzier.ad@gmail.com>
2025-06-25 09:18:35 +0200

    PatientEntity implemented

---
+++ patientService/src/main/java/com/openclassrooms/medilabo/patientService/model/PatientEntity.java
@@ -0,0 +1,37 @@
+package com.openclassrooms.medilabo.patientService.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+@Entity
+public class PatientEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "First name is mandatory")
+    private String firstName;
+
+    @NotBlank(message = "Last name is mandatory")
+    private String lastName;
+
+    @NotBlank(message = "Date of birth is mandatory")
+    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Date of birth must be in the format YYYY-MM-DD")
+    private String dateOfBirth;
+
+    @NotBlank(message = "Gender is mandatory")
+    @Pattern(regexp = "F|M", message = "Gender must be either 'F' or 'M'")
+    private String gender;
+
+    @Pattern(regexp = "\\d{3}-\\d{3}-\\d{4}", message = "Phone number must be in the format xxx-xxx-xxxx")
+    private String phoneNumber;
+
+    private String address;
+}
