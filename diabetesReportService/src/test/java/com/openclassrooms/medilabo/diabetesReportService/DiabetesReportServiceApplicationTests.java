f7d8ef9b00201c6d82b9e5edd11650f2eb08785e
Cy\Cy <marion.lauzier.ad@gmail.com>
2025-07-11 16:00:19 +0200

    initial structure

---
+++ diabetesReportService/src/test/java/com/openclassrooms/medilabo/diabetesReportService/DiabetesReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.medilabo.diabetesReportService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabetesReportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
